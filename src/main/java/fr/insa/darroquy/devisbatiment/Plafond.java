43477aa5bfdc7784ff971b7aedcb79d2f4b55d27
xdarroquy01 <xdarroquy01@sec-c212-f15.csip.in>
2023-03-29 18:52:58 +0200

    nouvelle classe plafond encore incomplète

---
+++ src/main/java/fr/insa/darroquy/devisbatiment/Plafond.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.darroquy.devisbatiment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author xdarroquy01
+ */
+public class Plafond {
+    int idPlafond;
+    ArrayList<Coin>listeCoin;
+    ArrayList<Revetements> listeRevetements;
+    
+    Plafond(ind id, ArrayList<Coin>coin, ArrayList<Revetements> revetements )
+    {
+        this.idPlafond=id;
+        this.listeCoin=coin;
+        this.listeRevetements=revetements;
+    }
+     double longueur()
+    {
+        return();
+    }
+     
+      double surface()
+    {
+        System.out.println("\nEntrer la Hauteur du mur (hauteur sous-plafond)");
+        double hsp=Lire.d();
+        return();
+    }
+}
